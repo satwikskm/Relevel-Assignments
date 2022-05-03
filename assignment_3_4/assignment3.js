617e56da306a84b5dc1128851c10b465f34c241a
S@TwIt <satwikmohantyskm@gmail.com>
2022-05-03 23:14:01 +0530

    assignment2,3

---
+++ assignment_3_4/assignment3.js
@@ -0,0 +1,15 @@
+for(let i=0;i<=100;i++){
+    if(i%3===0){
+        console.log("Relevel")
+    }
+    else if(i%5===0){
+        console.log("Buzz")
+    }
+
+    if(i%15===0){
+        console.log("6indBuzz")
+    }
+    else{
+        console.log(i)
+    }
+}
---
+++ assignment_3_4/assignment4.js
@@ -0,0 +1,8 @@
+let num = parseInt(prompt("enter a number"))
+
+if(num%2 === 0){
+    console.log("Odd")
+}
+else{
+    console.log("Even")
+}
