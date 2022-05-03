29b61bc7f87ecf62aa194d09641cb191928a0486
S@TwIt <satwikmohantyskm@gmail.com>
2022-05-03 22:43:09 +0530

    assignment1,2

---
+++ calculator/calculator.js
@@ -0,0 +1,23 @@
+'use strict'
+
+let val1=prompt("Enter first Number");
+
+let val2=prompt("Enter second Number");
+
+let option = prompt("Enter operation add,sub,mul,div");
+
+if(option === 'add'){
+    console.log(val1+val2)
+}
+else if(option === 'add'){
+    console.log(val1+val2)
+}
+else if(option === 'sub'){
+    console.log(val1-val2)
+}
+else if(option === 'mul'){
+    console.log(val1*val2)
+}
+else if(option === 'div'){
+    console.log(val1/val2)
+}
---
+++ calculator/working.js
@@ -0,0 +1,7 @@
+let var1=10;
+let var2=10;
+
+console.log("addition",var1+var2);
+console.log("subtraction",var1-var2)
+console.log("multiplication",var1*var2)
+console.log("division",var1/var2)
